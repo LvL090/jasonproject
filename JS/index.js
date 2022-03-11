b91af150b331967ad2d78cc036df02b81c9e1c61
LvL090 <calvod879@gmail.com>
2022-03-11 10:25:44 +0100

    update 3

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,5 @@
 setTimeout(function(){
     window.location='./secondpage.html'
     }, 5000);
+
+
--- JS/secondpage.js
+++ JS/secondpage.js
@@ -1,12 +1,26 @@
-const requestURL = './json/peliculas.json';
+const requestURL = "./JSON/peliculas.json";
 
 async function fetchMoviesJSON(){
     const response = await fetch(requestURL);
     const movies = await response.json();
     return movies;
-}
-
+};
 fetchMoviesJSON().then(movies => {
-    console.log(movies);
+    //console.log(movies);
 
-})
+    for (let index =0; index < movies.peliculas.length; index++) {
+
+        let img = movies.peliculas[index].img;
+        let title = movies.peliculas[index].title;
+        let rating = movies.peliculas[index].rating;
+        
+        films.innerHTML += `
+        <div class="poster1">
+                <img class="poster" src="${img}" alt="">
+                <p class="title">${title}</p>
+                <p class="rating">${rating}</p>
+        </div> 
+        `
+    }
+}
+)
